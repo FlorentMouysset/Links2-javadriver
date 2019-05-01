4d5f5197f1555c0a8f4c5bfd7a0d4e6f50924745
florent.mouysset <florent.mouysset@BLPP-5743.mshome.net>
2019-05-01 11:37:58 +0200

    adding attributes map to Relation

--- Links2-javadriver/src/links2/driver/model/Relation.java
+++ Links2-javadriver/src/links2/driver/model/Relation.java
@@ -1,5 +1,9 @@
 package links2.driver.model;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Objects;
 
 public class Relation {
@@ -9,17 +13,16 @@ public class Relation {
 	private String nodeBid;
 	private Boolean isOriented;
 	private String type;
+    private Map<String, Object> attributeMap;
+
 
 	public Relation(String relationID, Entity entity1, Entity entity2, boolean isOriented, String type) {
-		this.relationID = relationID;
-		this.nodeAid = entity1.getEntityID();
-		this.nodeBid = entity2.getEntityID();
-		this.isOriented = isOriented;
-		this.type = type;
+        this(relationID, entity1.getEntityID(), entity2.getEntityID(), isOriented, type);
 	}
 
 	public Relation(String relationID, String nodeAid, String nodeBid, boolean isOriented, String type) {
-		this.relationID = relationID;
+        this();
+        this.relationID = relationID;
 		this.nodeAid = nodeAid;
 		this.nodeBid = nodeBid;
 		this.isOriented = isOriented;
@@ -27,9 +30,9 @@ public class Relation {
 	}
 
 	public Relation() {
+        attributeMap = new HashMap<>();
 	}
 
-	// private Attri
 	public String getRelationID() {
 		return relationID;
 	}
@@ -70,32 +73,62 @@ public class Relation {
 		this.type = type;
 	}
 
-	@Override
-	public String toString() {
-		return "Relation [relationID=" + relationID + ", nodeAid=" + nodeAid + ", nodeBid=" + nodeBid + ", isOriented="
-				+ isOriented + ", type=" + type + "]";
-	}
+    public Map<String, Object> getAttributeMap() {
+        return new HashMap<>(attributeMap);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(isOriented, nodeAid, nodeBid, relationID, type);
-	}
+    public void setAttributeMap(Map<String, Object> attributeMap) {
+        this.attributeMap = new HashMap<>(attributeMap);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Relation other = (Relation) obj;
-		return Objects.equals(isOriented, other.isOriented) && Objects.equals(nodeAid, other.nodeAid)
-				&& Objects.equals(nodeBid, other.nodeBid) && Objects.equals(relationID, other.relationID)
-				&& Objects.equals(type, other.type);
-	}
+    public void setAttribute(String attributeName, Object attributeValue) {
+        if (null == attributeMap) {
+            attributeMap = new HashMap<>();
+        }
+        attributeMap.put(attributeName, attributeValue);
+    }
+
+    @Override
+    public String toString() {
+        final int maxLen = 5;
+        return "Relation [relationID=" + relationID + ", nodeAid=" + nodeAid + ", nodeBid=" + nodeBid + ", isOriented="
+                + isOriented + ", type=" + type + ", attributeMap="
+                + (attributeMap != null ? toString(attributeMap.entrySet(), maxLen) : null) + "]";
+    }
+
+    private String toString(Collection<?> collection, int maxLen) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        int i = 0;
+        for (Iterator<?> iterator = collection.iterator(); iterator.hasNext() && i < maxLen; i++) {
+            if (i > 0)
+                builder.append(", ");
+            builder.append(iterator.next());
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attributeMap, isOriented, nodeAid, nodeBid, relationID, type);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Relation other = (Relation) obj;
+        return Objects.equals(attributeMap, other.attributeMap) && Objects.equals(isOriented, other.isOriented)
+                && Objects.equals(nodeAid, other.nodeAid) && Objects.equals(nodeBid, other.nodeBid)
+                && Objects.equals(relationID, other.relationID) && Objects.equals(type, other.type);
+    }
 
 }
